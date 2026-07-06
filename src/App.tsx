f243bddfef27a9c11077ad4f6d7e8d3715428750
smimilen <smimilen@users.noreply.github.com>
2026-07-06 02:07:25 +0000

    Physics easter egg: floating football toggle, cards tumble with matter-js gravity, drag & toss, Esc/X to exit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import HeroVideo from './components/HeroVideo'
 import ProjectsGrid from './components/ProjectsGrid'
 import Footer from './components/Footer'
+import PhysicsMode from './components/PhysicsMode'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <ProjectsGrid />
       </main>
       <Footer />
+      <PhysicsMode />
     </>
   )
 }
